ae13f81b825ccd615841e65493e3e59a55396fe9
rapando <samsonrapando@gmail.com>
2023-11-28 22:46:56 +0300

    using a different release method

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(AddNumbers(4, 5))
-	fmt.Println(IsGreaterThan5(6))
+	fmt.Println(IsGreaterThan5(6)
 }
 
 func AddNumbers(x, y int) int {
